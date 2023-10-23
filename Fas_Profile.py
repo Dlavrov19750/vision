f5cb4cbd049e068be19d13bd4779ed89d641e8bb
Дмитрий <75394990+Dlavrov19750@users.noreply.github.com>
2023-10-23 12:32:57 +0300

    Add files via upload

---
+++ Fas_Profile.py
@@ -0,0 +1,76 @@
+import cv2
+import sys
+import numpy as np
+
+capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Загрузите видеофайл, камера:
+# свойства захвата камеры, при видиофайле игнорируются.
+capture.set(cv2.CAP_PROP_FPS, 24)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 900)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+
+# player = MediaPlayer(video_path)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+id = input("Введите свой номер:_")
+# id = int(id)
+count = 0
+# Кадр - это изображение, которое вы хотите, флаг - это успех / неудача:
+
+
+# Перебирайте кадры видео:
+
+
+while True:
+	flag, frame = capture.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+
+	for (x, y, w, h) in faces:
+		count += 1
+		cv2.imwrite('dataset/User.' + str(id) + "." + str(count) + ".jpg", gray[y:y + h, x:x + w])
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	# audio_frame, val = player.get_frame()
+
+		if flag == 0:
+			break
+
+		# Escape to exit
+	if count > 25:
+		break
+
+
+	cv2.waitKey(1) & 0xFF
+	cv2.imshow("FAS", frame)
+
+cv2.destroyAllWindows()
+#----------------------------------------------------------------------------------------------------------------------
+ids = int(id)
+count = 26
+face_profile_cascade = cv2.CascadeClassifier(r'haarcascade_profileface.xml')
+while True:
+	flag, frame = capture.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces_prof = face_profile_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+	for (x, y, w, h) in faces_prof:
+		count += 1
+		cv2.imwrite('dataset/User.' + str(ids) + "." + str(count) + ".jpg", gray[y:y + h, x:x + w])
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+# audio_frame, val = player.get_frame()
+
+		if flag == 0:
+			break
+
+
+# Escape to exit
+	if count > 52:
+		break
+
+	cv2.waitKey(1) & 0xFF
+	cv2.imshow("Profile", frame)
+
+capture.release()
+cv2.destroyAllWindows()
+
+print("Все Данные профиля собраны")
+# _______________________________________________________________________________________________________________________
